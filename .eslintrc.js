3225c2052a89e2687328c1bde8adf2d7ad2d1721
Meer Sagor <meersagor.contact@gmail.com>
2023-03-01 12:20:07 +0600

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:prettier/recommended",
+
+    "plugin:vue/strongly-recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint", "prettier"],
+  rules: {
+    "no-console": "error",
+  },
+}
---
+++ components/YourName.vue
@@ -0,0 +1,10 @@
+<template>
+  <Html>
+    <Head>
+      <Title>Your Name</Title>
+    </Head>
+    <Body>
+      <h1>Your Name</h1>
+    </Body>
+  </Html>
+</template>
